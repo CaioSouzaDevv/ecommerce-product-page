b65e39ff86c49af4e78104386a3b2f34db8cff0a
Caiosouzadevv <caioss20.cs@gmail.com>
2025-10-18 11:08:16 -0300

    feat: concatenating counting function directly into cart

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -6,6 +6,9 @@ let counterAmount = document.querySelector('input[type="number"]')
 const addToCart = document.querySelector('.btn-cart');
 const btnCart = document.querySelector('.cart');
 const boxCart = document.querySelector('.box-cart');
+let contentCar = document.querySelector('.box-cart__alert');
+let titleProduct = document.querySelector('.product__title');
+let valueProduct = document.querySelector('.product__value');
 gallerySub.forEach(img => {
 
     img.addEventListener('click', function (e) {
@@ -64,3 +67,26 @@ btnCart.addEventListener('click', function (e) {
         boxCart.style.display = "block";
     }
 })
+
+
+addToCart.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    if (counterAmount.value <= 0) {
+        contentCar.querySelector('.box-cart__item').textContent = `Your cart is empty.`;
+    } else {
+        const valueTitle = titleProduct.textContent;
+        const imgSrc = galleryMain.src;  // <- aqui é necessário
+        let priceNumber = parseFloat(valueProduct.textContent.replace('$', '').trim());
+        let totalSum = priceNumber * Number(counterAmount.value); contentCar.innerHTML = `
+          <img src="${imgSrc}" alt="${valueTitle}" width="40" height="40">
+          <p>${valueTitle} x ${counterAmount.value} <br><strong>$:${totalSum.toFixed(2)}</strong></p>
+        `;
+    }
+  if (boxCart.style.display === "block") {
+        boxCart.style.display = "none";
+    } else {
+        boxCart.style.display = "block";
+    }
+    counterAmount.value = 0;
+});
